d6a7bbe60da8ceb401db5c9c84197dea2729e31e
choisojin <sojn6093@gmail.com>
2024-07-02 22:41:30 +0900

    first commit

---
+++ firstboot_ncp/src/main/java/com/example/demo/FirstbootApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/*1. 내장 tomcat 시작(8080->9090), 
+ * 2. component-scan 자동 동작 설정, 
+ 3. src/main/respources/application.properties
+ //  /WEB-INF/views/*.jsp 경로와 확장자 사용자추가설정
+ 
+ 
+//스프링부트클래스 실행 - run as - spring boot app (선택!!!!) / java application (main) / run on server(mvc, jsp, servlet)
+*/
+@SpringBootApplication //실행
+
+
+@ComponentScan(basePackages = "boardmapper")
+@MapperScan(basePackages = "boardmapper")
+@ComponentScan(basePackages = "websocket")
+public class FirstbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstbootApplication.class, args);
+	}
+
+}
